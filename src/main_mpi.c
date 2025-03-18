2a87acb0c7bbd3a17531770ac0509c86c6d7de31
JohnnyVenturas <bogdanbudura@gmail.com>
2025-03-18 12:41:27 +0100

    Finalizing MPI+OpenMP

--- src/main_mpi.c
+++ src/main_mpi.c
@@ -676,76 +676,63 @@ void apply_blur_filter(animated_gif *image, int size, int threshold) {
 }
 
 void apply_sobel_filter(animated_gif *image) {
-    int i, j, k;
-    int width, height;
+    #pragma omp parallel  // Create persistent team of threads
+    {
+        for (int i = 0; i < image->n_images; i++) {
+            const int width = image->width[i];
+            const int height = image->height[i];
+            pixel *pixels = image->p[i];
+            pixel *sobel = malloc(width * height * sizeof(pixel));
 
-    pixel **p;
+            // Sobel computation with 2D parallelization and vectorization
+            #pragma omp for collapse(2) schedule(guided) nowait
+            for (int j = 1; j < height - 1; j++) {
+                #pragma omp simd aligned(pixels, sobel : 64)
+                for (int k = 1; k < width - 1; k++) {
+                    const int conv_idx = CONV(j, k, width);
+                    
+                    // Load neighbor pixels using direct memory access
+                    const int offsets[] = {
+                        CONV(j-1, k-1, width), CONV(j-1, k, width), CONV(j-1, k+1, width),
+                        CONV(j+1, k-1, width), CONV(j+1, k, width), CONV(j+1, k+1, width),
+                        CONV(j, k-1, width), conv_idx, CONV(j, k+1, width)
+                    };
+                    
+                    const int b_no = pixels[offsets[0]].b;
+                    const int b_n  = pixels[offsets[1]].b;
+                    const int b_ne = pixels[offsets[2]].b;
+                    const int b_so = pixels[offsets[3]].b;
+                    const int b_s  = pixels[offsets[4]].b;
+                    const int b_se = pixels[offsets[5]].b;
+                    const int b_o  = pixels[offsets[6]].b;
+                    const int b_e  = pixels[offsets[8]].b;
 
-    p = image->p;
+                    // Compute gradients using integer arithmetic
+                    const float deltaX = -b_no + b_ne - 2*b_o + 2*b_e - b_so + b_se;
+                    const float deltaY = b_se + 2*b_s + b_so - b_ne - 2*b_n - b_no;
+                    const float val = sqrtf(deltaX*deltaX + deltaY*deltaY) / 4.0f;
 
-    for (i = 0; i < image->n_images; i++) {
-        width = image->width[i];
-        height = image->height[i];
-
-        pixel *sobel;
-
-        sobel = (pixel *)malloc(width * height * sizeof(pixel));
-
-        for (j = 1; j < height - 1; j++) {
-            for (k = 1; k < width - 1; k++) {
-                int pixel_blue_no, pixel_blue_n, pixel_blue_ne;
-                int pixel_blue_so, pixel_blue_s, pixel_blue_se;
-                int pixel_blue_o, pixel_blue, pixel_blue_e;
-
-                float deltaX_blue;
-                float deltaY_blue;
-                float val_blue;
-
-                pixel_blue_no = p[i][CONV(j - 1, k - 1, width)].b;
-                pixel_blue_n = p[i][CONV(j - 1, k, width)].b;
-                pixel_blue_ne = p[i][CONV(j - 1, k + 1, width)].b;
-                pixel_blue_so = p[i][CONV(j + 1, k - 1, width)].b;
-                pixel_blue_s = p[i][CONV(j + 1, k, width)].b;
-                pixel_blue_se = p[i][CONV(j + 1, k + 1, width)].b;
-                pixel_blue_o = p[i][CONV(j, k - 1, width)].b;
-                pixel_blue = p[i][CONV(j, k, width)].b;
-                pixel_blue_e = p[i][CONV(j, k + 1, width)].b;
-
-                deltaX_blue = -pixel_blue_no + pixel_blue_ne -
-                              2 * pixel_blue_o + 2 * pixel_blue_e -
-                              pixel_blue_so + pixel_blue_se;
-
-                deltaY_blue = pixel_blue_se + 2 * pixel_blue_s + pixel_blue_so -
-                              pixel_blue_ne - 2 * pixel_blue_n - pixel_blue_no;
-
-                val_blue = sqrt(deltaX_blue * deltaX_blue +
-                                deltaY_blue * deltaY_blue) /
-                           4;
-
-                if (val_blue > 50) {
-                    sobel[CONV(j, k, width)].r = 255;
-                    sobel[CONV(j, k, width)].g = 255;
-                    sobel[CONV(j, k, width)].b = 255;
-                } else {
-                    sobel[CONV(j, k, width)].r = 0;
-                    sobel[CONV(j, k, width)].g = 0;
-                    sobel[CONV(j, k, width)].b = 0;
+                    // Branchless thresholding using bitwise operations
+                    const unsigned char result = (val > 50.0f) ? 255 : 0;
+                    sobel[conv_idx] = (pixel){result, result, result};
                 }
             }
-        }
 
-        for (j = 1; j < height - 1; j++) {
-            for (k = 1; k < width - 1; k++) {
-                p[i][CONV(j, k, width)].r = sobel[CONV(j, k, width)].r;
-                p[i][CONV(j, k, width)].g = sobel[CONV(j, k, width)].g;
-                p[i][CONV(j, k, width)].b = sobel[CONV(j, k, width)].b;
+            // Wait for all threads to finish computation before updating pixels
+            #pragma omp barrier
+
+            // Parallel pixel update with vectorization
+            #pragma omp for simd collapse(2) schedule(static) aligned(pixels, sobel : 64)
+            for (int j = 1; j < height - 1; j++) {
+                for (int k = 1; k < width - 1; k++) {
+                    pixels[CONV(j, k, width)] = sobel[CONV(j, k, width)];
+                }
             }
-        }
 
-        free(sobel);
+            free(sobel);
+        }
     }
 }
-
 /*
  * Main entry point
  */
